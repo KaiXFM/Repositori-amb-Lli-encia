7bfffb6705f081ce0ca12c8de861e0c5aa8b86d4
KaiXFM <102524875+KaiXFM@users.noreply.github.com>
2023-05-25 15:36:04 +0200

    Add files via upload

---
+++ HolaMon.java
@@ -0,0 +1,23 @@
+//Exemple de Hola Mon
+
+public class HolaMon
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hola DAM!");
+
+		System.out.println("Adeu DAM");
+
+		//Declaracio de variable
+
+		int edad;
+
+		//Asignar valor a una variable
+
+		edad=40;
+
+		//edad te el valor de 40
+
+		System.out.println("Mi edad es: " + edad);
+	}
+}
